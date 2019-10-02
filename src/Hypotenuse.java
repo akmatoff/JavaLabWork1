daf92f3aba8d9b16f43a679efaeb07b95cb39aaa
akmatoff <azim.akmatov1@gmail.com>
2019-10-02 21:08:12 +0600

    Initial Commit

---
+++ src/Hypotenuse.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Hypotenuse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите значение для катета a: ");
+        double kateta = scanner.nextInt();
+        System.out.println("Введите значение для катета b: ");
+        double katetb = scanner.nextInt();
+
+        // Вычисляю гипотенузу треугольника
+        double katetasquare = kateta*kateta;
+        double katetbsquare = katetb*katetb;
+        double sum = katetasquare + katetbsquare;
+        double hypotenuse = Math.sqrt(sum);
+
+        // Вычисляю значение угла треугольника
+        double divide = kateta / katetb;
+        double tan = Math.tan(divide);
+        double todegree = tan * (180 / Math.PI);
+
+        // Вычисляю высоту треугольника
+        double p = (kateta + katetb + hypotenuse) / 2;
+        double a = p * (p - kateta) * (p - katetb) * (p - hypotenuse);
+        double adivide = a / kateta;
+        double h = (Math.sqrt(a) * 2) / kateta;
+
+        // Вычисляю площадь треугольника
+        double S = (kateta * h) / 2;
+
+        // Вычисляю периметр треугольника
+        double P = kateta + katetb + hypotenuse;
+
+        System.out.println("Гипотенуза треугольника равна: " + hypotenuse + " см");
+        System.out.println("Значение угла равно: " + todegree + " градусов");
+        System.out.println("Высота треугольника равна: " + h + " см");
+        System.out.println("Площадь треугольника равна: " + S + " см в квадрате");
+        System.out.println("Периметр треугольника равна: " + P + " см");
+
+    }
+}
---
+++ src/Pyramid.java
@@ -0,0 +1,10 @@
+public class Pyramid {
+    public static void main(String[] args) {
+        for(int row = 1; row <= 4; row++) {
+            for(int i = 1; i <= 7; i++) {
+                System.out.print(" " + i);
+            }
+            System.out.println();
+        }
+    }
+}
