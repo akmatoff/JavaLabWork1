07cfa2195b4f793ad2f9798a49dcaaed9cfcb60a
akmatoff <azim.akmatov1@gmail.com>
2019-10-06 15:15:34 +0600

    Built the rocket

--- src/Rocket.java
+++ src/Rocket.java
@@ -3,30 +3,98 @@
  */
 
 public class Rocket {
-    public static void main(String[] args) {
+    // Top of the rocket
+    static void top(int size) {
 
-        int row, sym, space, topstar;
+        int row, sym, space;
 
-        // Left part of the rocket's top
         for(row = 5; row >=1; row--) {
-            for(space = 0; space <= row; space++) {
+            for(space = 1; space <= row; space++) {
                 System.out.print(" "); // Space before every string
             }
 
             for(sym = 5; sym >= row; sym--) {
                 System.out.print("/"); // Print the symbols
             }
+            System.out.print("**");
+            for(sym = 5; sym >= row; sym--) {
+                System.out.print("\\");
+            }
             System.out.println();
         }
+    }
 
-        // Middle part of the rocket's top
-        for (row = 5; row >= 1; row--) {
-            for (topstar = 2; topstar >= 1; topstar--) {
-                System.out.print("*");
+    // Body of the rocket
+    static void body(int size) {
+        int row, sym, x;
+
+        // Plus and equal symbols
+        for(row = 0; row < 1; row++) {
+            for(sym = 6; sym >= 0; sym--) {
+                System.out.print("+=");
             }
             System.out.println();
         }
 
+        // Middle part
+        for(row = 6; row >= 1; row--) {
+            for(sym = 6; sym >= 0; sym--) {
+                System.out.print("|.");
+            }
+            System.out.println();
+        }
+
+
+        // Plus and equal symbols
+        for(row = 0; row < 1; row++) {
+            for(sym = 6; sym >= 0; sym--) {
+                System.out.print("+=");
+            }
+            System.out.println();
+        }
+
+        // Middle part
+        for(row = 6; row >= 1; row--) {
+            for(sym = 6; sym >= 0; sym--) {
+                System.out.print("|.");
+            }
+            System.out.println();
+        }
+
+        // Plus and equal symbols
+        for(row = 0; row < 1; row++) {
+            for(sym = 6; sym >= 0; sym--) {
+                System.out.print("+=");
+            }
+            System.out.println();
+        }
+
+    }
+
+    // Bottom of the rocket
+    static void bottom(int size) {
+        int row, sym,space;
+
+        for(row = 5; row >= 1; row--) {
+            for(space = 1; space <= row; space++) {
+                System.out.print(" ");
+            }
+            for(sym = 5; sym >= row; sym--) {
+                System.out.print("/");
+            }
+            System.out.print("**");
+            for(sym = 5; sym >= row; sym--) {
+                System.out.print("\\");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        top(3);
+        body(3);
+        bottom(3);
 
         // The end of the main method
     }
